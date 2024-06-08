c970436879763e2abe94543fa86053923c7a89fb
abdirahmanbare <abdirahmanhusseinbare@gmail.com>
2024-06-08 17:00:33 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution in this file!
+let companyName = 'Scuber';
+const companyName1 = "scuber"
+let mostProfitableNeighborhood = "Chelsea"
+const companyCeo = "Susan Smith"
+let companyCeo1 = "Susan smith"
